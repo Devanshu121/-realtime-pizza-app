a97e60ee2396f19e274d9768dbf6f32301edfe64
Devanshu121 <devanshujaiswal121@gmail.com>
2023-01-16 22:32:53 +0530

    add cart page, auth

--- server.js
+++ server.js
@@ -7,15 +7,32 @@ const PORT = process.env.PORT || 3000    // in place of 3000 you can app any por
 
 //Assets
 app.use(express.static('public'));
-app.get('/',(req, res) =>{
-    res.render('home')
-})
+
+
 
 //set template engine
 
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+// Routes
+app.get('/',(req, res) =>{
+    res.render('home')
+})
+
+app.get('/cart',(req, res)=>{
+    res.render('customers/cart')
+})
+
+app.get('/login',(req, res) =>{
+    res.render('auth/login')
+})
+
+app.get('/register',(req, res) =>{
+    res.render('auth/register')
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`server is running on hello port ${PORT}`)
