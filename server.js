b6b1471b09f64602c7d60d4a0db60d39420050c4
Devanshu121 <devanshujaiswal121@gmail.com>
2023-01-18 23:04:00 +0530

    add to cart

--- server.js
+++ server.js
@@ -1,37 +1,61 @@
+require('dotenv').config()
 const express = require('express');
 const app = express()
 const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000    // in place of 3000 you can app any port you want like-3300 or 3333 or 4000 etc..
+const mongoose = require('mongoose');
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')
 
+//Database connection
+mongoose.set('strictQuery',false);
+const url = 'mongodb://localhost:27017/pizza';
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log('Database connected...');
+}).on('error',(err)=> {
+    console.log('Connection failed...')
+});
+
+// Session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: new MongoDbStore({
+        // Session store
+        mongoUrl:'mongodb://localhost:27017/pizza',
+        mongooseConnection: connection, 
+        collection: 'sessions' 
+    }),
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
+}))     
+
+
+app.use(flash())
 //Assets
 app.use(express.static('public'));
+app.use(express.json())
 
-
+// Global middleware
+app.use((req, res, next) => {
+    res.locals.session = req.session
+    // res.locals.user = req.user
+    next()
+})
 
 //set template engine
-
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
 // Routes
-app.get('/',(req, res) =>{
-    res.render('home')
-})
+require('./routes/web')(app)
 
-app.get('/cart',(req, res)=>{
-    res.render('customers/cart')
-})
-
-app.get('/login',(req, res) =>{
-    res.render('auth/login')
-})
-
-app.get('/register',(req, res) =>{
-    res.render('auth/register')
-})
 
 
 app.listen(PORT, ()=>{
