ca015454075d93791ad21eea462fe7422358f75c
Devanshu121 <devanshujaiswal121@gmail.com>
2023-01-20 20:34:55 +0530

    socket integration

--- routes/web.js
+++ routes/web.js
@@ -3,6 +3,7 @@ const cartController = require('../app/http/controllers/customers/cartController
 const orderController = require('../app/http/controllers/customers/orderController')
 const adminOrderController = require('../app/http/controllers/admin/orderController')
 const homeController = require('../app/http/controllers/homeController')
+const statusController = require('../app/http/controllers/admin/statusController')
 
 // Middlewares 
 const guest = require('../app/http/middleware/guest')
@@ -23,11 +24,12 @@ function initRoutes(app){
     // Customer routes
     app.post('/orders', auth, orderController().store)
     app.get('/customer/orders', auth, orderController().index)
-    // app.get('/customer/orders/:id', auth, orderController().show)
+    app.get('/customer/orders/:id', auth, orderController().show)
 
     // Admin routes
     app.get('/admin/orders', admin, adminOrderController().index)
-    // app.post('/admin/order/status', admin, statusController().update)
+    app.post('/admin/order/status', admin, statusController().update)
+
 }
 
 module.exports = initRoutes
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')
 const passport = require('passport')
+const Emitter = require('events')
 
 //Database connection
 mongoose.set('strictQuery',false);
@@ -21,6 +22,10 @@ connection.once('open', () => {
 }).on('error',(err)=> {
     console.log('Connection failed...')
 });
+
+// Event emitter
+const eventEmitter = new Emitter()
+app.set('eventEmitter', eventEmitter)
 
 // Session config
 app.use(session({
@@ -65,6 +70,24 @@ require('./routes/web')(app)
 
 
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on hello port ${PORT}`)
+const server = app.listen(PORT , () => {
+            console.log(`Listening on port ${PORT}`)
+        })
+
+// Socket
+
+const io = require('socket.io')(server)
+io.on('connection', (socket) => {
+    // Join
+    socket.on('join', (roomName) => {
+        socket.join(roomName)
+    })
+})
+
+eventEmitter.on('orderUpdated', (data) => {
+    io.to(`order_${data.id}`).emit('orderUpdated', data)
+})
+
+eventEmitter.on('orderPlaced', (data) => {
+    io.to('adminRoom').emit('orderPlaced', data)
 })
